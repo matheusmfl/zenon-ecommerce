4ec28621aca8be6918e56668053a66bb7277b53e
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-20 14:22:25 -0300

    fix: ensure product prices are wrapped one line

--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -9,9 +9,9 @@ interface ProductItemProps {
 
 export function ProductItem({ product }: ProductItemProps) {
   return (
-    <div className="flex flex-col gap-4 w-[156px]">
+    <div className="flex flex-col gap-4 w-[170px]">
       {/* img div */}
-      <div className="relative bg-accent rounded-[10px] h-[170px]  flex items-center justify-center ">
+      <div className="relative bg-accent rounded-[10px] h-[170px] w-[170px]  flex items-center justify-center ">
         <Image src={product.imageUrls[0]} alt="Img generic" width={0} height={0} sizes={"100vw"}
           className="h-auto w-auto object-contain max-w-[80%] max-h-[70%]" />
 
@@ -27,16 +27,16 @@ export function ProductItem({ product }: ProductItemProps) {
         <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis w-full">
           {product.name}
         </p>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ">
 
           {product.discountPercentage > 0 && (
 
             <>
-              <p className="font-semibold">
+              <p className="font-semibold overflow-hidden whitespace-nowrap text-ellipsis w-full">
                 {product.totalPrice.toFixed(2)}
               </p>
 
-              <p className="opacity-75 line-through text-xs">
+              <p className="opacity-75 line-through text-xs overflow-hidden whitespace-nowrap text-ellipsis w-full">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
