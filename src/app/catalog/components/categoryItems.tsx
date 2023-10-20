78c28efdceb1dbf54d462c488d67ab86df58a8ea
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-20 14:49:48 -0300

    chore: create background-items on tailwind configs, feat: add Categories Items Components

---
+++ src/app/catalog/components/categoryItems.tsx
@@ -0,0 +1,27 @@
+import { Category } from "@prisma/client"
+import Image from "next/image"
+
+interface CategoryItemsProps {
+  category: Category
+}
+
+export function CategoriesItem({ category }: CategoryItemsProps) {
+  return (
+    <div className="flex flex-col">
+      {/* img Div */}
+      <div className="w-full h-[150px] flex items-center justify-center
+       bg-item-gradients">
+        <Image src={category.imageUrl} alt={category.name} width={0} height={0} sizes={"100vw"}
+          className="h-auto w-auto object-contain max-w-[80%] max-h-[70%]" />
+
+      </div>
+
+      {/* Text Div */}
+      <div className="bg-accent py-3 rounded-br-lg rounded-bl-lg">
+        <p className="text-sm font-semibold text-center">
+          {category.name}
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,24 @@
-export default function Catalog() {
-  return (
-    <div>
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import { CategoriesItem } from "./components/categoryItems";
 
+export default async function Catalog() {
+  const categories = await prismaClient.category.findMany()
+  return (
+    <div className="p-5 flex flex-col gap-8">
+      <Badge className="gap-1 w-fit text-base uppercase border-primary border-2 py-[0.375rem] px-3" variant={"outline"}>
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => {
+          return (
+            <CategoriesItem key={category.id} category={category} />
+          )
+        })}
+      </div>
     </div>
   )
 }
