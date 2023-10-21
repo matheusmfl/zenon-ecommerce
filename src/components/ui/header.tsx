e655d02443ce322bccf4e4c833ad694a8c9221f9
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-21 11:15:52 -0300

    feat: ensure user goes to homepage on header logo link

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -103,11 +103,14 @@ export function Header() {
           </SheetContent>
         </Sheet>
 
-        <h1 className="text-lg font-semibold ">
-          <span className="text-primary">
-            ZenonTech
-          </span> Store
-        </h1>
+        <Link href={'/'}>
+          <h1 className="text-lg font-semibold ">
+            <span className="text-primary">
+              ZenonTech
+            </span> Store
+          </h1>
+        </Link>
+
 
         <Button size={"icon"} variant={"outline"}>
           <ShoppingCartIcon />
